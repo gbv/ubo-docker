58e246d6a0c321512ebb20aee6a51cbdc5af9422
Sebastian Hofmann <hofmann.s@uni-jena.de>
2020-04-15 13:31:42 +0200

    Initial commit

---
+++ Dockerfile
@@ -0,0 +1,35 @@
+# Bind paths /root/.mycore/ubo/
+FROM alpine/git as git
+ARG UBO_BRANCH=issues/UBO-38-Integrate_SOLR_7_configuration
+RUN mkdir /opt/ubo
+WORKDIR /opt/
+RUN git --version && \
+    git clone https://github.com/MyCoRe-Org/ubo.git
+WORKDIR /opt/ubo
+RUN git checkout ${UBO_BRANCH}
+
+FROM maven:3-jdk-11 as maven
+RUN groupadd maven && \
+    useradd -m -g maven maven
+USER maven
+COPY --from=git --chown=maven:maven /opt/ubo/ /opt/ubo
+WORKDIR /opt/ubo
+RUN mvn --version && \
+    mvn clean install -Djetty -DskipTests && \
+    rm -rf ~/.m2
+
+FROM tomcat:8-jre11
+EXPOSE 8080
+EXPOSE 8009
+USER root
+WORKDIR /usr/local/tomcat/
+ARG PACKET_SIZE="65536"
+ENV JAVA_OPTS="-Xmx1g -Xms1g"
+ENV APP_CONTEXT="ubo"
+COPY docker-entrypoint.sh /usr/local/bin/ubo.sh
+RUN ["chmod", "+x", "/usr/local/bin/ubo.sh"]
+RUN rm -rf /usr/local/tomcat/webapps/*
+RUN cat /usr/local/tomcat/conf/server.xml | sed "s/\"AJP\/1.3\"/\"AJP\/1.3\" packetSize=\"$PACKET_SIZE\"/g" > /usr/local/tomcat/conf/server.xml.new
+RUN mv /usr/local/tomcat/conf/server.xml.new /usr/local/tomcat/conf/server.xml
+COPY --from=maven --chown=root:root /opt/ubo/ /opt/ubo/
+CMD ["bash", "/usr/local/bin/ubo.sh"]
---
+++ docker-entrypoint.sh
@@ -0,0 +1,66 @@
+#!/usr/bin/bash
+set -e
+
+# Following Variables needed:
+#
+# JDBC_NAME - The Username of the Database
+# JDBC_PASSWORD - The Password of the User
+# JDBC_DRIVER - The diver to use for JDBC
+# JDBC_URL - The URL to use for JDBC
+# APP_CONTEXT - The context of the webapp
+# SOLR_URL - The url to the solr server
+# SOLR_CORE - The name of the main solr core
+
+echo "UBO Starter Script"
+sleep 5 # wait for database (TODO: replace with wait-for-it)
+MCR_HOME=/root/.mycore/${APP_CONTEXT}/
+
+function downloadDriver {
+  FILENAME=$(basename $1)
+  if [[ ! -f "${MCR_HOME}lib/$FILENAME" ]]
+  then
+    curl -o "${MCR_HOME}lib/$FILENAME" "$1"
+  fi
+  if [[ ! -f "/opt/ubo/target/${FILENAME}" ]]
+  then
+    cp "${MCR_HOME}lib/$FILENAME" "/opt/ubo/target/${FILENAME}"
+  fi
+}
+
+function setUpMyCoRe {
+    JAVA_OPTS="-DMCR.AppName=${APP_CONTEXT}" /opt/ubo/target/bin/ubo.sh create configuration directory
+
+    sed -ri "s/(name=\"javax.persistence.jdbc.user\" value=\").*(\")/\1${JDBC_NAME}\2/" "${MCR_HOME}resources/META-INF/persistence.xml"
+    sed -ri "s/(name=\"javax.persistence.jdbc.password\" value=\").*(\")/\1${JDBC_PASSWORD}\2/" "${MCR_HOME}resources/META-INF/persistence.xml"
+    sed -ri "s/(name=\"javax.persistence.jdbc.driver\" value=\").*(\")/\1${JDBC_DRIVER}\2/" "${MCR_HOME}resources/META-INF/persistence.xml"
+    sed -ri "s/(name=\"javax.persistence.jdbc.url\" value=\").*(\")/\1${JDBC_URL}\2/" "${MCR_HOME}resources/META-INF/persistence.xml"
+    #sed -ri "s/(name=\"hibernate.hbm2ddl.auto\" value=\").*(\")/\1update\2/" "${MCR_HOME}resources/META-INF/persistence.xml"
+    sed -ri "s/<mapping-file>META-INF\/mycore-viewer-mappings.xml<\/mapping-file>//" "${MCR_HOME}resources/META-INF/persistence.xml"
+    sed -ri "s/#?(MCR\.Solr\.ServerURL=).+/\1${SOLR_URL}/" "${MCR_HOME}mycore.properties"
+    sed -ri "s/#?(MCR\.Solr\.ServerURL=).+/\1${SOLR_URL}/" "${MCR_HOME}mycore.properties"
+    sed -ri "s/#?(MCR\.Solr\.Core\.main\.Name=).+/\1${SOLR_CORE}/" "${MCR_HOME}mycore.properties"
+    sed -ri "s/#?(MCR\.Solr\.Core\.main\.Name=).+/\1${SOLR_CORE}/" "${MCR_HOME}mycore.properties"
+    mkdir -p "${MCR_HOME}lib"
+
+    case $JDBC_DRIVER in
+      org.postgresql.Driver) downloadDriver "https://jdbc.postgresql.org/download/postgresql-42.2.9.jar";;
+      org.mariadb.jdbc.Driver) downloadDriver "https://repo.maven.apache.org/maven2/org/mariadb/jdbc/mariadb-java-client/2.5.4/mariadb-java-client-2.5.4.jar";;
+      org.hsqldb.jdbcDriver) downloadDriver "https://repo.maven.apache.org/maven2/org/hsqldb/hsqldb/2.5.0/hsqldb-2.5.0.jar";;
+      org.h2.Driver) downloadDriver "https://repo.maven.apache.org/maven2/com/h2database/h2/1.4.200/h2-1.4.200.jar";;
+      com.mysql.jdbc.Driver) downloadDriver "https://repo.maven.apache.org/maven2/mysql/mysql-connector-java/8.0.19/mysql-connector-java-8.0.19.jar";;
+    esac
+
+    JAVA_OPTS="-DMCR.AppName=${APP_CONTEXT}" /opt/ubo/target/bin/ubo.sh init superuser
+    JAVA_OPTS="-DMCR.AppName=${APP_CONTEXT}" /opt/ubo/target/bin/ubo.sh update all classifications from directory /opt/ubo/src/main/setup/classifications
+    JAVA_OPTS="-DMCR.AppName=${APP_CONTEXT}" /opt/ubo/target/bin/ubo.sh update permission create-mods for id POOLPRIVILEGE with rulefile src/main/resources/acl-rule-always-allowed.xml described by always allowed
+    JAVA_OPTS="-DMCR.AppName=${APP_CONTEXT}" /opt/ubo/target/bin/ubo.sh update permission read for id default with rulefile /opt/ubo/src/main/resources/acl-rule-always-allowed.xml described by always allowed
+    JAVA_OPTS="-DMCR.AppName=${APP_CONTEXT}" /opt/ubo/target/bin/ubo.sh update permission read for id restapi:/ with rulefile /opt/ubo/src/main/resources/acl-rule-always-allowed.xml described by always allowed
+    JAVA_OPTS="-DMCR.AppName=${APP_CONTEXT}" /opt/ubo/target/bin/ubo.sh update permission read for id restapi:/ with rulefile /opt/ubo/src/main/resources/acl-rule-always-allowed.xml described by always allowed
+    JAVA_OPTS="-DMCR.AppName=${APP_CONTEXT}" /opt/ubo/target/bin/ubo.sh reload solr configuration main in core main
+}
+
+[ "$(ls -A "$MCR_HOME")" ] && echo "MyCoRe-Home is not empty" || setUpMyCoRe
+
+rm -rf /usr/local/tomcat/webapps/*
+cp /opt/ubo/target/ubo-*.war "/usr/local/tomcat/webapps/${APP_CONTEXT}.war"
+catalina.sh run
